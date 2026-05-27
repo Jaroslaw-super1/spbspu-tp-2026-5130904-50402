1e57cf4f32e2c77d78ba4b826c940f7c5453926f
jar <Jarafan3@gmail.com>
2026-05-27 19:33:01 +0300

    Add operators

--- afanasev.yaroslav/T2/DataStruct.hpp
+++ afanasev.yaroslav/T2/DataStruct.hpp
@@ -11,7 +11,53 @@ namespace afanasev
 {
   class DataStruct
   {
+    unsigned long long key1;
+    std::pair< long long, unsigned long long > key2;
+    std::string key3;
   };
+
+  struct CharExpect
+  {
+    char exp;
+  };
+
+  struct StringMatch
+  {
+    std::string exp;
+  };
+
+  struct ULLIn
+  {
+    unsigned long long & num;
+  };
+
+  struct RacionalNum
+  {
+    std::pair< long long, unsigned long long > & num;
+  };
+
+  struct kavichki
+  {
+    std::string & str;
+  };
+
+  std::istream & operator>>(std::istream & in, DataStruct & str);
+  std::ostream & operator<<(std::ostream & out, const DataStruct & str);
+
+  std::istream & operator>>(std::istream & in, CharExpect && str);
+  std::istream & operator>>(std::istream & in, StringMatch && str);
+  std::istream & operator>>(std::istream & in, ULLIn && str);
+  std::istream & operator>>(std::istream & in, RacionalNum && str);
+  std::istream & operator>>(std::istream & in, kavichki && str);
 }
+
+std::istream & afanasev::operator>>(std::istream & in, DataStruct & str);
+std::ostream & afanasev::operator<<(std::ostream & out, const DataStruct & str);
+
+std::istream & afanasev::operator>>(std::istream & in, CharExpect && str);
+std::istream & afanasev::operator>>(std::istream & in, StringMatch && str);
+std::istream & afanasev::operator>>(std::istream & in, ULLIn && str);
+std::istream & afanasev::operator>>(std::istream & in, RacionalNum && str);
+std::istream & afanasev::operator>>(std::istream & in, kavichki && str);
 
 #endif
